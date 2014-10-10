a91d795f743e6db571f854ad60c05bf6406673d2
Shuaib Mohammad <shuaibm@gmail.com>
2014-10-10 12:04:11 -0400

    Added comments and copyright notice

--- smartlink.php
+++ smartlink.php
@@ -1,3 +1,13 @@
+/* 
+Copyright Time at Task Aps 2013. All rights reserved. David Andersen owns all the code you create for this project.
+
+The purpose of this script is to handle renamed smartlinks, call the real ultradox link and redirect the user to a target url.
+This is done by substituting building a URL to call by appending the "id" query string parameter to "www.ultradox.com"
+
+The script expects a query string parameter called "id" 
+The script calls ultradox with this "id" (which was the original link before renaming) and redirects the user to a target url.
+*/
+
 <?php
 //Rebuild original ultradox url using the 'id' parameter in the query string
 $URL = "http://www.ultradox.com".htmlspecialchars($_GET["id"]);
